1db75bc81a9219f965bee5043338d2aa56786aa3
cpmoore0317 <cpmoore317@gmail.com>
2024-10-02 16:39:38 -0500

    Implemented data fetching

--- components/TopicLinks.tsx
+++ components/TopicLinks.tsx
@@ -1,17 +1,14 @@
+import { fetchTopics } from "@/lib/data";
 import TopicLink from "./TopicLink";
 
-const topics = [
-  { id: "1", title: "C#" },
-  { id: "2", title: "Python" },
-  { id: "3", title: "Golang" },
-];
-
 export default async function TopicLinks() {
+  const topics = await fetchTopics(); // Fetch topics from the database
+
   return (
     <>
-      {topics.map((topic) => {
-        return <TopicLink key={topic.id} id={topic.id} title={topic.title} />;
-      })}
+      {topics.map((topic) => (
+        <TopicLink key={topic.id} id={topic.id} title={topic.title} />
+      ))}
     </>
   );
 }
